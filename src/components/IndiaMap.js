013b90bb35af9d2dd36bc4855c68f6505d8fc4dc
MehulKanani556 <mkanani556@gmail.com>
2024-12-14 09:43:33 +0530

    map pin

--- src/components/IndiaMap.js
+++ src/components/IndiaMap.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import img from '../Images/pin.png'
 
 // Use built-in Leaflet icons
 const defaultIcon = new L.Icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    // iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    // iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    iconUrl: img,
     shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
     iconSize: [25, 41], // size of the icon
     iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
